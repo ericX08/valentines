e82797e7bee5ee626add815999e8c46165789ed7
ericX08 <eric.xiao312@gmail.com>
2024-02-05 22:31:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import { Button } from "antd";
+import "./App.css";
+import SecondPage from "./second-page";
 
-function App() {
+const MainPage = () => {
+  const [noButtonClicks, setNoButtonClicks] = useState(0);
+  const navigate = useNavigate();
+
+  const handleNoClick = () => {
+    setNoButtonClicks((prevClicks) => prevClicks + 1);
+  };
+
+  const handleYesClick = () => {
+    navigate("./second-page");
+  };
+
+  const yesButtonStyle = {
+    height: `${75 + 75 * noButtonClicks}px`,
+    width: `${100 + 100 * noButtonClicks}px`,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="main-page">
+      <h1>Will you be my valentine?</h1>
+      <div className="button-container">
+        <Button
+          className="yes-button"
+          style={yesButtonStyle}
+          onClick={handleYesClick}
         >
-          Learn React
-        </a>
-      </header>
+          Yes
+        </Button>
+        <Button className="no-button" onClick={handleNoClick}>
+          No
+        </Button>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/second-page" element={<SecondPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/second-page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import gif from './valentines.gif';
+
+
+const SecondPage = () => {
+    return (
+      <div className="second-page">
+        <img src={gif} alt="Valentine's Gif" />
+      </div>
+    );
+  };
+
+  export default SecondPage;
+  
